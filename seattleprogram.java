eb89b7a0f3055617c06e01173a23e36ccfc48cd5
Karthik <ksaikarthik2@gmail.com>
2024-08-28 09:32:32 -0500

    This is a feature branch created for testing seattle

---
+++ seattleprogram.java
@@ -0,0 +1,9 @@
+class seattleprogram
+{
+        public static void main(String a[])
+        {
+			System.out.print("This edit is to see the seattle -b branch");
+			System.out.print("we can see the branch seattle feature");
+        }
+
+}
